eeb9e911a4d98a9effa9ba565642945382bebfb1
brh28 <31115595+brh28@users.noreply.github.com>
2022-11-17 09:28:28 -0600

    Update reviews table with text wrap and AuthorTag (#45)

--- src/controls/articles/view/ReadReviews.jsx
+++ src/controls/articles/view/ReadReviews.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Table, SIZE, DIVIDER } from "baseui/table-semantic";
-
+import {
+  TableBuilder,
+  TableBuilderColumn,
+} from 'baseui/table-semantic';
+import AuthorTag from '../../common/AuthorTag'
 const options = { year: 'numeric', month: 'short', day: 'numeric' };   
 
 const ReadReviews = ({reviews}) => {
 	if (!reviews) return null
 	else return (
-		<div style={{marginTop: '20px', marginBottom: '20px'}}>
-			<Table
-				columns={["User", 
-					"Date", 
-					"Review" 
-					]}
-				data={reviews.map((r, idx) => [
-						r.user_id,
-						new Date(r.timestamp).toLocaleDateString('en-US', options),
-						r.value 
-					]) 
-				}
-				size={SIZE.compact}
-	      		divider={DIVIDER.grid} />
-      	</div>
+		<TableBuilder
+		  overrides={{Root: {style: {marginTop: '20px', marginBottom: '20px', whiteSpace: 'pre-wrap'}}}}
+		  data={reviews}
+		>
+			<TableBuilderColumn 
+				header='Reviews'
+			  	overrides={{TableHeadCell: {style: {width: '15%'}}}}>
+		        {r => new Date(r.timestamp).toLocaleDateString('en-US', options)}
+		    </TableBuilderColumn>
+			<TableBuilderColumn 
+				overrides={{TableHeadCell: {style: {width: '15%'}}}}
+				>
+				{r => (
+				  <AuthorTag authorId={r.user_id} />
+				)}
+			</TableBuilderColumn>
+			<TableBuilderColumn>
+				{r => r.value}
+			</TableBuilderColumn>
+	    </TableBuilder>
 	)
 }
 
